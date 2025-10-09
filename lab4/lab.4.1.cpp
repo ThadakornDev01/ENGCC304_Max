c3adfe3c6cb640319229fd3cbcc6f6f473c8f10d
ThadakornDev01 <Thadakon2004@gmail.com>
2025-10-09 22:09:09 +0700

    Add files via upload

---
+++ lab4/lab.4.1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int days, seconds;
+    printf("Input Days : ");
+    scanf("%d", &days);
+    seconds = days * 24 * 60 * 60;
+    printf("%d days = %d seconds\n", days, seconds);
+    return 0;
+}
